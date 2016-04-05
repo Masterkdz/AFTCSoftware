40426ba440b9966aed3ad00e4d31148e6a7fc5b9
Kévin DE ZORZI <Kvin DE ZORZI>
2016-04-05 11:20:33 +0200

    ActionsDBmodif

--- src/actions/actionsDB.go
+++ src/actions/actionsDB.go
@@ -70,10 +70,10 @@ func AddResult(db *sql.DB,) ([]string, error) {
 		error := errors.New("db = nil ")
 		return result, error
 	}
-	stmt, err := db.Prepare("INSERT INTO usagers(nom, prenom) VALUES(?,?)")
+	stmt, err := db.Prepare("INSERT INTO usagers(nom, prenom, email, Auteur_fiche, Date, NomTCCL, telephone_fixe, Portable, CirconstancesTrauma, ConditionsPriseEnchargeMedicale, ConditionsPriseEnChargeSociale, DemarchesAdministrativesJuridiquesEffectuees, ProblemesRecontres, AttentesAFTC, Observations) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")
     checkErr(err)
 
-    res, err := stmt.Exec("DEZORZI", "Kevin") //Remplacer les valeurs par les variables corresepondantes
+    res, err := stmt.Exec("", "", "", "", "", "", "", "", "", "", "", "", "", "", "") //Remplacer les valeurs par les variables corresepondantes
     checkErr(err)
 
     id, err := res.LastInsertId()
@@ -84,3 +84,32 @@ func AddResult(db *sql.DB,) ([]string, error) {
 	db.Close()
 	return result, nil
 }
+
+func DisplayResult(db *sql.DB,) ([]string, error) {
+	var result []string
+	db, err := sql.Open("mysql", "Root@/dbAFTC.db3")
+    checkErr(error)
+	if db == nil {
+		err := errors.New("db = nil ")
+		return result, err
+	}
+	rows, err := db.Query("SELECT nom, prenom, email, Auteur_fiche, Date, NomTCCL, telephone_fixe, Portable, CirconstancesTrauma, ConditionsPriseEnchargeMedicale, ConditionsPriseEnChargeSociale, DemarchesAdministrativesJuridiquesEffectuees, ProblemesRecontres, AttentesAFTC, Observations FROM usagers")
+    checkErr(err)
+
+    for rows.Next() {
+        var nom string
+        var prenom string
+        err = rows.Scan(&nom, &prenom, &email, &Auteur_fiche, &Date, &NomTCCL, &telephone_fixe, &Portable, &CirconstancesTrauma, &ConditionsPriseEnchargeMedicale, aConditionsPriseEnChargeSociale, &DemarchesAdministrativesJuridiquesEffectuees, &ProblemesRecontres, &AttentesAFTC, &Observations)
+        checkErr(err)
+        fmt.Println(nom)
+        fmt.Println(prenom)
+    }
+
+	if err != nil {
+        panic(err)
+    }
+    
+	db.Close()
+	return result, nil
+}
+    
