8675509f0fbebe3f0a8b5d476a92c235ede0e30f
Sylver <sylverimhoff@gmail.com>
2016-04-17 10:21:11 +0200

    fix static rendering

--- src/main.go
+++ src/main.go
@@ -1,24 +1,54 @@
-package main 
+/*package main 
 
 import (
-	//"./handlers"
+	"./handlers"
 	"log"
 	"net/http"
-	//"html/template"
-	//"./router"
-	//"fmt"
+	"html/template"
+	"./router"
+	"fmt"
 )
 
-//func handler(w http.ResponseWriter, r *http.Request) {
-//    fmt.Fprintf(w, "test", r.URL.Path[1:])
-//}
+func handler(w http.ResponseWriter, r *http.Request) {
+   fmt.Fprintf(w, "test", r.URL.Path[1:])
+}
 
 func main() {
-	//router := router.NewRouter()
+	router := router.NewRouter()
 	fs := http.FileServer(http.Dir("template"))
   	http.Handle("/", fs)
-  	//http.HandleFunc("/handler", test.Test)
+  	http.HandleFunc("/handler", test.Test)
 
 	log.Println("API listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))//router))
+	log.Fatal(http.ListenAndServe(":8080", nil))router))
+}*/
+
+package main
+
+import (
+  "html/template"
+  "log"
+  "net/http"
+  "path"
+)
+
+func main() {
+  //fs := http.FileServer(http.Dir("template"))
+  //http.Handle("./template/", http.StripPrefix("./template/", fs))
+
+  http.HandleFunc("/", serveTemplate)
+
+  log.Println("Listening...")
+  http.ListenAndServe(":8080", nil)
+}
+
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+  lp := path.Join("./template", "index.html")
+  //fp := path.Join("	./template", r.URL.Path)
+
+  tmpl, err := template.ParseFiles(lp)
+  if err != nil {
+  	log.Println(err)
+  }
+  tmpl.ExecuteTemplate(w, "index.html", nil)
 }
